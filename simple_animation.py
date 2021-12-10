23edf091225a217053637a9a4bdeb42f20a6d4ef
Lorenzo S <73533964+s08302317@users.noreply.github.com>
2021-12-10 13:06:08 -0500

    v0.8
    Draw the box onto the game surface,
    
    Draw window to the screen

--- simple_animation.py
+++ simple_animation.py
@@ -1,4 +1,4 @@
-# Simple Animation with PyGame, Lorenzo Saliard, 12/10/21, 1:01, v0.7
+# Simple Animation with PyGame, Lorenzo Saliard, 12/10/21, 1:05, v0.8
 
 import pygame, sys, time
 from pygame.locals import *
@@ -81,4 +81,11 @@ while True:
                     b['dir'] = DOWNLEFT
                 if b['dir'] == UPRIGHT:
                     b['dir'] = UPLEFT
+        # Draw the box onto the game surface.
+        pygame.draw.rect(windowSurface, b['color'], b['rect'])
+
+
+    # Draw the window to the screen.
+    pygame.display.update()
+    time.sleep(0.02)
             
