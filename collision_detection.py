1c37e2b218853bec1912c8c81927e45246b897a2
Lorenzo S <73533964+s08302317@users.noreply.github.com>
2022-01-24 12:58:41 -0500

    v1.0 and v1.1 update
    Check to see if player collides with food.
    
    Draw food on the screen

--- collision_detection.py
+++ collision_detection.py
@@ -1,4 +1,4 @@
-# PyGame Collision Detection Practice, Lorenzo Saliard, January 24, 2022, 12:48pm, v0.9
+# PyGame Collision Detection Practice, Lorenzo Saliard, January 24, 2022, 12:57pm, v1.0 and 1.1
 
 import pygame, sys, random
 from pygame.locals import *
@@ -86,3 +86,24 @@ while True:
     # Draw whgite background on Window Surface.
     windowSurface.fill(WHITE)
 
+    # Move the player.
+    if moveDown and player.bottom < WINDOWHEIGHT:
+        player.top += MOVESPEED
+    if moveUp and player.top > 0:
+        pplayer.top -= MOVESPEED
+    if moveLeft and player.left > 0:
+        player.left -= MOVESPEED
+    if moveRight and player.right < WINDOWWIDTH:
+        player.right += MOVESPEED
+    
+    # Draw the player on the surface.
+    pygame.draw.rect(windowSurface, BLACK, player)
+
+    # Check for player colliding with food(s).
+    for food in foods[:]:
+        if.player colliderect(food):
+            foods.remove(food)
+
+    # Draw the food
+    for i in range(len(foods)):
+        pygame.draw.rect(windowSurface, GREEN, foods[i])
