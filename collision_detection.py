508efd8df71e9918f363119f1ee2a0d114efde95
Lorenzo S <73533964+s08302317@users.noreply.github.com>
2022-01-24 12:03:29 -0500

    v0.5
    Setup movement variables.

--- collision_detection.py
+++ collision_detection.py
@@ -1,4 +1,4 @@
-# PyGame Collision Detection Practice, Lorenzo Saliard, January 24, 2022, 11:59am, v0.4
+# PyGame Collision Detection Practice, Lorenzo Saliard, January 24, 2022, 12:02pm, v0.5
 
 import pygame, sys, random
 from pygame.locals import *
@@ -28,3 +28,10 @@ foods = []
 for i in range(20):
     foods.append(pygame.Rect(random.randint(0, WINDOWWIDTH - FOODSIZE), random.randint(0, WINDOWHEIGHT - FOODSIZE), FOODSIZE, FOODSIZE))
 
+# Movement Variables
+moveLeft = False
+moveRight = False
+moveUp = False
+moveDown = False
+
+MOVESPEED = 6 
