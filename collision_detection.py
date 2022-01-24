b9d2dc8221f974d505c9dced21382e4cfac7e33f
Lorenzo S <73533964+s08302317@users.noreply.github.com>
2022-01-24 12:42:53 -0500

    v0.8
    Created MOUSEBUTTONUP event to add food to the game.

--- collision_detection.py
+++ collision_detection.py
@@ -1,4 +1,4 @@
-# PyGame Collision Detection Practice, Lorenzo Saliard, January 24, 2022, 12:37pm, v0.7
+# PyGame Collision Detection Practice, Lorenzo Saliard, January 24, 2022, 12:42pm, v0.8
 
 import pygame, sys, random
 from pygame.locals import *
@@ -73,3 +73,7 @@ while True:
             if event.key == k_x:
                 player.top = random.randint(0, WINDOWHEIGHT - player.height)
                 player.left = random.randint(0, WINDOWWIDTH - player.width)
+
+        if event.type == MOUSEBUTTONUP:
+            foods.append(pygame.Rect(event.pos[0], event.pos[1], FOODSIZE, FOODSIZE))
+
