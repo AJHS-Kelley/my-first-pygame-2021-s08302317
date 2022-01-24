e786f1a2b539b4e7ce2129b80e3dd170d2ad6719
Lorenzo S <73533964+s08302317@users.noreply.github.com>
2022-01-24 12:00:32 -0500

    v0.4
    Created player and food data structures.

--- collision_detection.py
+++ collision_detection.py
@@ -1,4 +1,4 @@
-# PyGame Collision Detection Practice, Lorenzo Saliard, January 24, 2022, 11:53am, v0.3
+# PyGame Collision Detection Practice, Lorenzo Saliard, January 24, 2022, 11:59am, v0.4
 
 import pygame, sys, random
 from pygame.locals import *
@@ -17,3 +17,14 @@ pygame.display.set_caption('collision Detection 2022')
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
+
+# Setupd the player and food data structures.
+foodCounter = 0
+NEWFOOD = 40
+FOODSIZE = 20
+player = py.game.Rect(300, 100, 50, 50)
+foods = []
+
+for i in range(20):
+    foods.append(pygame.Rect(random.randint(0, WINDOWWIDTH - FOODSIZE), random.randint(0, WINDOWHEIGHT - FOODSIZE), FOODSIZE, FOODSIZE))
+
